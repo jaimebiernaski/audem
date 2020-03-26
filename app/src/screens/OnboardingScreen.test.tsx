ac851bc283866ca6f5e71c485ab65f498f54598f
Jaime Biernaski <>
2020-03-26 11:16:43 -0300

    Add string tranlation to title

--- app/src/screens/OnboardingScreen.test.tsx
+++ app/src/screens/OnboardingScreen.test.tsx
@@ -1,10 +1,16 @@
+/**
+ * OnboardingScreen.test.tsx
+ */
+
 import React from 'react';
 import { render } from 'react-native-testing-library';
 import { OnboardingScreen } from './OnboardingScreen';
 
+let t = jest.fn();
+
 describe('Onboarding Screen Tests', () => {
   test('render welcome message', () => {
-    const { queryByText } = render(<OnboardingScreen />);
-    expect(queryByText('Welcome')).toBeTruthy();
+    const { queryByTestId } = render(<OnboardingScreen />);
+    expect(queryByTestId('title').props.children).toBe('welcome');
   });
 });
